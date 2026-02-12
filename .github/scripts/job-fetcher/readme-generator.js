0964b83e5f6552782f19271a63d1d3bd3cc497cf
z-apply <admin@zapply.jobs>
2026-02-12 11:07:06 -0600

    fix: Use correct header image in readme-generator
    - Changed from sej-heading.png to nsj-heading.png
    - Changed title to Nursing Jobs 2026
    - Ensures next workflow run uses correct branding

--- .github/scripts/job-fetcher/readme-generator.js
+++ .github/scripts/job-fetcher/readme-generator.js
@@ -332,9 +332,9 @@ async function generateReadme(
   return `<div align="center">
 
 <!-- Banner -->
-<img src="images/sej-heading.png" alt="Software Engineering Jobs 2026 - Illustration of people working on tech.">
+<img src="images/nsj-heading.png" alt="Nursing Jobs 2026 - Illustration of healthcare professionals.">
 
-# Software Engineering Jobs 2026
+# Nursing Jobs 2026
 
 <!-- Row 1: Job Stats (Custom Static Badges) -->
 ![Total Jobs](https://img.shields.io/badge/Total_Jobs-${currentJobs.length}-brightgreen?style=flat&logo=briefcase) ![Companies](https://img.shields.io/badge/Companies-${totalCompanies}-blue?style=flat&logo=building) ${faangJobs > 0 ? '![FAANG+ Jobs](https://img.shields.io/badge/FAANG+_Jobs-' + faangJobs + '-red?style=flat&logo=star) ' : ''}![Updated](https://img.shields.io/badge/Updated-Every_15_Minutes-orange?style=flat&logo=calendar)
